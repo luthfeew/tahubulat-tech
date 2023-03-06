d385f780986011bc82f2d401c35ef4a17a786e09
luthfeew <luthfeew@gmail.com>
2023-03-06 19:17:07 +0700

    Enable Audiences

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
 	import '../app.css';
 	import Header from './Header.svelte';
 	import Footer from './Footer.svelte';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="app min-h-screen flex flex-col">
