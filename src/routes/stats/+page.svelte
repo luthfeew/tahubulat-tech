f5cb1d670b4a69b7e6aef9a5e5b4807e44c104bd
luthfeew <luthfeew@gmail.com>
2023-03-07 21:33:56 +0700

    Player Stats update

--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -1,5 +1,23 @@
 <script>
-	import { Breadcrumb, BreadcrumbItem, Card, Button, Input, Spinner, Alert } from 'flowbite-svelte';
+	// @ts-nocheck
+
+	import {
+		Breadcrumb,
+		BreadcrumbItem,
+		Card,
+		Button,
+		Input,
+		Spinner,
+		Alert,
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell,
+		Listgroup,
+		Avatar
+	} from 'flowbite-svelte';
 
 	const endpoint = 'https://genshinapi-3-e7518255.deta.app/full/';
 	let promise = null;
@@ -24,7 +42,6 @@
 		loading = false;
 
 		if (res.ok) {
-			// console.log(data);
 			return data;
 		} else {
 			throw new Error(data);
@@ -68,14 +85,101 @@
 	<p>...waiting</p>
 {:then data}
 	{#if data}
-		{data.uid}
-		{data.data.info.nickname} <br />
-		{#each data.data.characters as item, i}
-			{i + 1}:{item.name} <br />
-		{/each}
-		{#each Object.entries(data.data.stats) as [key, value]}
-			{key}: {value} <br />
-		{/each}
+		<Card class="mb-4">
+			<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Info</h5>
+			<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+				<Table>
+					<TableBody>
+						{#each Object.entries(data.data.info) as [key, value]}
+							{#if value}
+								<TableBodyRow>
+									<TableBodyCell tdClass="p-1 whitespace-nowrap font-medium" style="width:60%"
+										>{key}</TableBodyCell
+									>
+									<TableBodyCell tdClass="p-1 whitespace-nowrap font-medium">{value}</TableBodyCell>
+								</TableBodyRow>
+							{/if}
+						{/each}
+						<TableBodyRow>
+							<TableBodyCell tdClass="p-1 whitespace-nowrap font-medium" style="width:60%"
+								>uid</TableBodyCell
+							>
+							<TableBodyCell tdClass="p-1 whitespace-nowrap font-medium">{data.uid}</TableBodyCell>
+						</TableBodyRow>
+					</TableBody>
+				</Table>
+			</p>
+		</Card>
+
+		<Card>
+			<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Stats</h5>
+			<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+				<Table>
+					<TableBody>
+						{#each Object.entries(data.data.stats) as [key, value]}
+							<TableBodyRow>
+								<TableBodyCell tdClass="p-1 whitespace-nowrap font-medium" style="width:60%"
+									>{key}</TableBodyCell
+								>
+								<TableBodyCell tdClass="p-1 whitespace-nowrap font-medium">{value}</TableBodyCell>
+							</TableBodyRow>
+						{/each}
+					</TableBody>
+				</Table>
+			</p>
+		</Card>
+
+		<Card padding="xl" size="md">
+			<div class="flex justify-between items-center mb-4">
+				<h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Characters</h5>
+			</div>
+			<Listgroup items={data.data.characters} let:item class="border-0 dark:!bg-transparent">
+				<div class="flex space-x-4">
+					<div class="flex flex-col space-y-4">
+						<Avatar src={item.icon} class="flex-shrink-0" rounded />
+						<Avatar src={item.weapon.icon} class="flex-shrink-0" rounded />
+					</div>
+					<div class="flex-1 min-w-0">
+						<Table>
+							<TableHead>
+								<TableHeadCell colspan="2">{item.name}</TableHeadCell>
+							</TableHead>
+							<TableBody>
+								{#each Object.entries(item) as [key, value], i}
+									{#if i > 2 && i < 10 && key !== 'icon' && key !== 'collab'}
+										<TableBodyRow color="blue">
+											<TableBodyCell tdClass="p-1 whitespace-nowrap font-medium" style="width:60%">
+												{key}
+											</TableBodyCell>
+											<TableBodyCell tdClass="p-1 whitespace-nowrap font-medium">
+												{value}
+											</TableBodyCell>
+										</TableBodyRow>
+									{/if}
+									{#if key === 'weapon'}
+										{#each Object.entries(value) as [key, value], i}
+											{#if i > 3 && key !== 'description'}
+												<TableBodyRow color="green">
+													<TableBodyCell
+														tdClass="p-1 whitespace-nowrap font-medium"
+														style="width:60%"
+													>
+														{key}
+													</TableBodyCell>
+													<TableBodyCell tdClass="p-1 whitespace-nowrap font-medium">
+														{value}
+													</TableBodyCell>
+												</TableBodyRow>
+											{/if}
+										{/each}
+									{/if}
+								{/each}
+							</TableBody>
+						</Table>
+					</div>
+				</div>
+			</Listgroup>
+		</Card>
 	{:else}
 		<p>EMPTY</p>
 	{/if}
