a41a8b94b5ae9590f7b88a7afb4a36c1f525d13e
luthfeew <luthfeew@gmail.com>
2023-03-07 18:32:02 +0700

    Player Stats WIP

--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -1,5 +1,35 @@
 <script>
-	import { Alert } from 'flowbite-svelte';
+	import { Breadcrumb, BreadcrumbItem, Card, Button, Input, Spinner, Alert } from 'flowbite-svelte';
+
+	const endpoint = 'https://genshinapi-3-e7518255.deta.app/full/';
+	let promise = null;
+	let value = '';
+	let loading = false;
+
+	$: disabled = value.length < 9;
+
+	function sanitize() {
+		value = value.replace(/[^0-9]/g, '').slice(0, 9);
+	}
+
+	function process() {
+		if (value.length < 9) return;
+		promise = getData();
+	}
+
+	async function getData() {
+		loading = true;
+		const res = await fetch(`${endpoint}${value}`);
+		const data = await res.json();
+		loading = false;
+
+		if (res.ok) {
+			// console.log(data);
+			return data;
+		} else {
+			throw new Error(data);
+		}
+	}
 </script>
 
 <svelte:head>
@@ -7,20 +37,48 @@
 	<meta name="description" content="Player Stats" />
 </svelte:head>
 
-<Alert border class="mt-4">
-	<span slot="icon"
-		><svg
-			aria-hidden="true"
-			class="w-5 h-5"
-			fill="currentColor"
-			viewBox="0 0 20 20"
-			xmlns="http://www.w3.org/2000/svg"
-			><path
-				fill-rule="evenodd"
-				d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-				clip-rule="evenodd"
-			/></svg
-		>
-	</span>
-	<span class="font-medium">WIP.</span> This page is still under construction.
+<div class="flex flex-col items-center justify-center">
+	<Breadcrumb class="my-4">
+		<BreadcrumbItem href="/" home>Home</BreadcrumbItem>
+		<BreadcrumbItem>Player Stats</BreadcrumbItem>
+	</Breadcrumb>
+</div>
+
+<Card class="mx-auto">
+	<form class="flex flex-col space-y-6" on:submit|preventDefault={process}>
+		<h3 class="text-xl font-medium text-gray-900 dark:text-white p-0">Player Stats</h3>
+		<Input id="search" placeholder="Enter UID..." size="lg" bind:value on:input={sanitize}>
+			<Button {disabled} slot="right" size="sm" on:click={process}>
+				{#if loading}
+					<Spinner size="5" />
+				{:else}
+					Show
+				{/if}
+			</Button>
+		</Input>
+	</form>
+</Card>
+
+<Alert border color="yellow" class="my-4">
+	<span class="font-medium">Warning alert!</span> This feature is still in development. Some data may
+	not be available.
 </Alert>
+
+{#await promise}
+	<p>...waiting</p>
+{:then data}
+	{#if data}
+		{data.uid}
+		{data.data.info.nickname} <br />
+		{#each data.data.characters as item, i}
+			{i + 1}:{item.name} <br />
+		{/each}
+		{#each Object.entries(data.data.stats) as [key, value]}
+			{key}: {value} <br />
+		{/each}
+	{:else}
+		<p>EMPTY</p>
+	{/if}
+{:catch error}
+	<p>NOT FOUND / User's data is not public.</p>
+{/await}
--- src/routes/uncensor/+page.svelte
+++ src/routes/uncensor/+page.svelte
@@ -9,6 +9,8 @@
 	$: disabled = !value;
 
 	function process() {
+		if (!value) return;
+
 		let result = '';
 
 		if (checked) {
@@ -35,13 +37,13 @@
 </div>
 
 <Card class="mx-auto">
-	<form class="flex flex-col space-y-6" action="/">
+	<form class="flex flex-col space-y-6" on:submit|preventDefault={process}>
 		<h3 class="text-xl font-medium text-gray-900 dark:text-white p-0">Bypass Text Censorship</h3>
 		<Input
 			type="text"
 			placeholder="Type here"
 			bind:value
-			on:keypress={() => (done = false)}
+			on:keydown={() => (done = false)}
 			color={done === true ? 'green' : 'base'}
 		/>
 		<Toggle size="small" bind:checked on:change={() => (done = false)}>Extended mode</Toggle>
